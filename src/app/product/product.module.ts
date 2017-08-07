738aa8cda77d3304225d4a0cff0280b3cda136e1
Harry <harry74323@gmail.com>
2017-08-07 15:40:27 +1000

    create productList

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent }
+    ])
   ],
-  declarations: []
+  declarations: [ProductListComponent]
 })
 export class ProductModule { }
