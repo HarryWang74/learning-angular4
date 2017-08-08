b6b7efb62a8ac7cf8f423c202a943d51ceb3e1a7
Harry <harry74323@gmail.com>
2017-08-08 10:14:52 +1000

    create product detail

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule} from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent }
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id',
+        component: ProductDetailComponent
+      }
     ])
   ],
-  declarations: [ProductListComponent]
+  declarations: [ProductListComponent, ProductDetailComponent]
 })
 export class ProductModule { }
