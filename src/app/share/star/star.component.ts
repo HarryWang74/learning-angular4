451f9702bf6fdb1766c09c081ff5cb4460be68cd
Harry <harry74323@gmail.com>
2017-08-11 11:49:40 +1000

    star width from parent

--- src/app/share/star/star.component.ts
+++ src/app/share/star/star.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
-
+export class StarComponent implements OnChanges {
+  @Input() rating: number;
+  starWidth: number;
+  
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges():void{
+    this.starWidth = this.rating * 86/5;
   }
 
 }
