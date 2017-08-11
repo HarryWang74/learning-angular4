2d42d26e5ad0d52a11c31a8022d69ada79f407fa
Harry <harry74323@gmail.com>
2017-08-11 11:37:37 +1000

    create star component

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { StarComponent } from './star/star.component';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { FormsModule } from '@angular/forms';
   exports : [
     CommonModule,
     FormsModule,
+    StarComponent,
   ],
-  declarations: []
+  declarations: [StarComponent]
 })
 export class ShareModule { }
